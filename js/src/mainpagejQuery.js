43f0375581f657e68d336827e03a0039939dd4c4
grace <gracejoo26@gmail.com>
2021-02-18 17:02:46 +0900

    leica camera introducing_slide

--- js/src/mainpagejQuery.js
+++ js/src/mainpagejQuery.js
@@ -8,7 +8,7 @@
     const indiLi = indiUl.children('li');
     const indiA = indiLi.children('a');
 
-
+    // == 할필요가 없었던 작업. slide clone은 오왼 버튼 있을 때 만들기
     const newMainPicLi = mainPicLi.eq(-1).clone(true);
     pictureBoxUl.prepend(newMainPicLi);
     const newLiLen = pictureBoxUl.children('li').length;
@@ -16,10 +16,12 @@
     pictureBoxUl.css({'position':'relative','left':'-100%','width':(newLiLen*100)+'%'});
     mainPicLi.css({'width':(100/newLiLen)+'%'});
 
-    let slideN = 0;
+    let slideN = 1;
     let permission = true;
-    let timed = 2000;      
+    let timed = 3000;      
            
+    //== ViewBox + indicator slide
+
     indiA.on('mouseenter focus click', function(e){
                 e.preventDefault();
                 
@@ -38,12 +40,26 @@
    let timer = setInterval(function(){
         indiLi.eq(slideN).addClass('on');
         indiLi.eq(slideN).siblings().removeClass('on');
-       pictureBoxUl.stop().animate({'marginLeft': ((slideN) * -100) +'%'});
+       pictureBoxUl.stop().animate({ 'marginLeft' : ((slideN) * -100) +'%' });
        slideN +=1;
        if(slideN >= 5){
         slideN = 0;
        }
    },timed);
    
+   
+   
+   //== 카메라 slide
+   
+   const camSlideUl = $('.slide');
+   let slideM = 0;
+   setInterval (function(){
+       camSlideUl.stop().animate({ 'marginLeft' : ((slideM) * -100+'%') });
+       slideM += 1;
+       if(slideM > 5 ){
+           slideM = 0;
+       }
+   },timed);
+
 
 })(jQuery);
