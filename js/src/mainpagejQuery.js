edba4577eec66cdc6b9281098d10a334fc05bf4f
grace <gracejoo26@gmail.com>
2021-02-19 17:26:15 +0900

    mainpage_fin except akademie schdule

--- js/src/mainpagejQuery.js
+++ js/src/mainpagejQuery.js
@@ -52,10 +52,12 @@
    //== 카메라 slide
    
    const camSlideUl = $('.slide');
+   const camLiLen = camSlideUl.children('li').length;
    let slideM = 0;
    const camSlideLiLen = camSlideUl.children('li').length;
    setInterval (function(){
        camSlideUl.stop().animate({ 'marginLeft' : ((slideM) * -645+'px') });
+       camSlideUl.css({'width':camLiLen * 100 + '%'});
        slideM += 1;
        if(slideM > camSlideLiLen-4 ){
            slideM = 0;
@@ -64,7 +66,32 @@
 
    //== AKADEMIE SCHDULE SCROLL BANNER
 
+   const classDiv = $('.leica_classBox');
    const classUl = $('.leica_classEachBox');
+   const classLi = classUl.children('li');
+
+  
+
+   classDiv.on("wheel", function (event) {
+       event.preventDefault();
+    // 이벤트 핸들러: window 객체에 이벤트가 발생하면 실행할 기능
+    // → 마우스 휠을 굴리면 window 객체에서 wheel 이벤트가 발생한다.
+
+    // 이벤트 객체의 deltaY 속성을 통해 마우스 휠 버튼을 어느 방향으로 얼마만큼 굴렸는지 확인
+    // → 마우스 휠 버튼을 아래쪽으로 굴렸을 때 : deltaY 속성은 양수
+    // → 마우스 휠 버튼을 위쪽으로 굴렸을 때 : deltaY 속성은 음수
+    console.log("deltaY = " + event.originalEvent.deltaY);
+    if (event.originalEvent.deltaY < 0) {
+        // wheeled up
+        classUl.animate({ scrollTop: -100 *5 +'px' });
+        
+      }
+      else {
+        // wheeled down
+        classUl.animate({ scrollTop: +100*5 + 'px' });
+      }
+});
+
 
 
 })(jQuery);
